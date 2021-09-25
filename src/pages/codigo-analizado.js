13111869981156d1e906e00643882302d3e2147c
miroagustin <miroagustin11@gmail.com>
2021-09-25 19:08:01 -0300

    se corrige el error al navegar la pagina sin direccionar

--- src/pages/codigo-analizado.js
+++ src/pages/codigo-analizado.js
@@ -5,16 +5,17 @@ import { Link } from "gatsby";
 
 
 const CodigoAnalizadoPage = (props) => {
-    let codigoAnalizado = props.location.state.codigoAnalizado;
+    let codigoAnalizado = props.location.state ? props.location.state.codigoAnalizado :  null;
     return(
     <Layout
         supertitulo="Resultado"
         titulo="Resultado del Analisis"
         subtitulo='Debajo encontrara los resultados del analisis del codigo Java. Si el metodo tiene un ⚠️ se recomienda modularizarlo.'>
         <Link to="/">Volver a Home</Link>
-        <h2>Porcentaje total de lineas con comentarios simples: {codigoAnalizado.porcentajeComentariosTotal}</h2>
+        
+        <h2>Porcentaje total de lineas con comentarios simples: {codigoAnalizado ? codigoAnalizado.porcentajeComentariosTotal : 0 + "%"}</h2>
         <ListaMetodos
-            metodos={codigoAnalizado.metodos} />
+            metodos={codigoAnalizado ? codigoAnalizado.metodos : []} />
     </Layout>)
 }
 
